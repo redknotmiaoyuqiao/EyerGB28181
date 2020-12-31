944ed23bab7c5d926c086c73cf2adc548b1c1bcb
yuqiaomiao <yuqiaomiao@sohu-inc.com>
2020-12-31 15:27:29 +0800

    Update init_linux.sh

--- init_linux.sh
+++ init_linux.sh
@@ -13,10 +13,30 @@ git clone https://gitee.com/redknot/Eyer3rdpart
 chmod -R 777 Eyer3rdpart
 
 
+cd ${basepath}/Eyer3rdpart/c-ares-1.17.1
+# ./configure --prefix=${basepath}/Eyer3rdpart/c-ares-1.17.1/c_ares_install --enable-static --enable-shared
+./configure --enable-static --enable-shared
+autoreconf -ivf
+make clean
+make -j4
+make install
+
+
+echo "+"
+echo "+"
+echo "+"
+echo "+"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+"
+echo "+"
+echo "+"
+echo "+"
+
 
 
 cd ${basepath}/Eyer3rdpart/libosip2-5.2.0
-./configure --prefix=${basepath}/Eyer3rdpart/libosip2-5.2.0/libosip2_install --enable-static --enable-shared
+# ./configure --prefix=${basepath}/Eyer3rdpart/libosip2-5.2.0/libosip2_install --enable-static --enable-shared
+./configure --enable-static --enable-shared
 autoreconf -ivf
 make clean
 make -j4
@@ -35,35 +55,17 @@ echo "+"
 
 
 
-cp -r ${basepath}/Eyer3rdpart/libosip2-5.2.0/libosip2_install/include/osip2 ${basepath}/Eyer3rdpart/libexosip2-5.2.0/include/osip2
-cp -r ${basepath}/Eyer3rdpart/libosip2-5.2.0/libosip2_install/include/osipparser2 ${basepath}/Eyer3rdpart/libexosip2-5.2.0/include/osipparser2
+# cp -r ${basepath}/Eyer3rdpart/libosip2-5.2.0/libosip2_install/include/osip2 ${basepath}/Eyer3rdpart/libexosip2-5.2.0/include/osip2
+# cp -r ${basepath}/Eyer3rdpart/libosip2-5.2.0/libosip2_install/include/osipparser2 ${basepath}/Eyer3rdpart/libexosip2-5.2.0/include/osipparser2
+# cd ${basepath}/Eyer3rdpart/libexosip2-5.2.0
+# mkdir libexosip2_install
+
+# cp -r ${basepath}/Eyer3rdpart/libosip2-5.2.0/libosip2_install/lib ${basepath}/Eyer3rdpart/libexosip2-5.2.0/libexosip2_install/lib
+
+
 cd ${basepath}/Eyer3rdpart/libexosip2-5.2.0
-mkdir libexosip2_install
-
-cp -r ${basepath}/Eyer3rdpart/libosip2-5.2.0/libosip2_install/lib ${basepath}/Eyer3rdpart/libexosip2-5.2.0/libexosip2_install/lib
-
-
-cd ${basepath}/Eyer3rdpart/libexosip2-5.2.0
-./configure --prefix=${basepath}/Eyer3rdpart/libexosip2-5.2.0/libexosip2_install --enable-static --enable-shared
-autoreconf -ivf
-make clean
-make -j4
-make install
-
-
-echo "+"
-echo "+"
-echo "+"
-echo "+"
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "+"
-echo "+"
-echo "+"
-echo "+"
-
-
-cd ${basepath}/Eyer3rdpart/c-ares-1.17.1
-./configure --prefix=${basepath}/Eyer3rdpart/c-ares-1.17.1/c_ares_install --enable-static --enable-shared
+# ./configure --prefix=${basepath}/Eyer3rdpart/libexosip2-5.2.0/libexosip2_install --enable-static --enable-shared
+./configure --enable-static --enable-shared
 autoreconf -ivf
 make clean
 make -j4
@@ -78,6 +80,6 @@ fi
 
 mkdir Lib
 
-cp -r Eyer3rdpart/libosip2-5.2.0/libosip2_install Lib/libosip2_install
-cp -r Eyer3rdpart/libexosip2-5.2.0/libexosip2_install Lib/libexosip2_install
-cp -r Eyer3rdpart/c-ares-1.17.1/c_ares_install Lib/c_ares_install
+# cp -r Eyer3rdpart/libosip2-5.2.0/libosip2_install Lib/libosip2_install
+# cp -r Eyer3rdpart/libexosip2-5.2.0/libexosip2_install Lib/libexosip2_install
+# cp -r Eyer3rdpart/c-ares-1.17.1/c_ares_install Lib/c_ares_install
